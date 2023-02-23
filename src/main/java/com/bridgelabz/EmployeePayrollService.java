c92959c5150ac968a5038e8c270a5bac45eb2553
shiwani <shiwanipaunikar1@gmail.com>
2023-02-23 00:03:56 -0800

    Added:demonstrate file operation

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,40 +1,20 @@
 package com.bridgelabz;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import java.nio.file.Path;
 public class EmployeePayrollService {
     public void empPayrollRead() throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\Dell\\Desktop\\text.txt"));
-        for (String line : lines) {
-
-        }
-
-    }
-
-    public void writeDataEmployeePayRoll() throws IOException {
-        ArrayList<String> data = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter ID");
-        data.add(sc.next());
-        System.out.println("Enter Name");
-        data.add(sc.next());
-        System.out.println("Enter Salary");
-        data.add(sc.next());
-        Files.write(Paths.get("C:\\Users\\Dell\\Desktop\\text.txt"),
-                data, StandardOpenOption.CREATE);
+        Path lines = Paths.get("C:\\Users\\Dell\\Desktop\\IoFiles");
+            Files.list(lines).filter(Files::isRegularFile).forEach(System.out::print);
+            Files.newDirectoryStream(lines).forEach(System.out::println);
+            Files.newDirectoryStream(lines, path -> path.toFile().isFile() && path.toString().startsWith("temp"));
 
     }
 
     public static void main(String[] args) throws IOException {
         EmployeePayrollService emp = new EmployeePayrollService();
-        emp.writeDataEmployeePayRoll();
         emp.empPayrollRead();
     }
 }
