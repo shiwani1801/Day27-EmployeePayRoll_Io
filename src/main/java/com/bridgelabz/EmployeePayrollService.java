c0480e21e8760dc0425399db7476b6e2f593a162
shiwani <shiwanipaunikar1@gmail.com>
2023-02-23 10:15:14 -0800

    Added:Create employee Payroll service to show no. of entries

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -1,22 +1,22 @@
 package com.bridgelabz;
 
-
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class EmployeePayrollService {
-    public void listFilesDirectory() throws IOException, InterruptedException {
+    public long listFilesDirectory() throws IOException, InterruptedException {
 
-        List<String> line = Files.readAllLines(Paths.get("C:\\Users\\Dell\\Desktop\\IoFiles\\shiva.txt"));
-        for (String lines : line) {
-            System.out.println(lines);
-        }
+        long enteries = 0;
+        enteries = Files.lines(new File("C:\\Users\\Dell\\Desktop\\IoFiles\\shiva.txt")
+                .toPath()).count();
+        System.out.println(enteries+"=count");
+        return enteries;
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        EmployeePayrollService obj = new EmployeePayrollService();
-        obj.listFilesDirectory();
+        EmployeePayrollService emp = new EmployeePayrollService();
+        emp.listFilesDirectory();
+
     }
 }
